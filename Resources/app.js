ca7f287a8e30f6a130e990fc60d4126e5ee3065b
Satoshi <sat.takahashi1979@gmail.com>
2012-10-13 11:02:28 +0900

    Add

---
+++ Resources/app.js
@@ -0,0 +1,93 @@
+// this sets the background color of the master UIView (when there are no windows/tab groups on it)
+Titanium.UI.setBackgroundColor('#000');
+
+// create tab group
+var tabGroup = Titanium.UI.createTabGroup();
+
+// create base UI tab and root window
+var win1 = Titanium.UI.createWindow({  
+    title:'Timeline',
+    backgroundColor:'#fff'
+});
+var tab1 = Titanium.UI.createTab({  
+    icon:'KS_nav_views.png',
+    title:'ツイート',
+    window:win1
+});
+
+var tableView = Titanium.UI.createTableView({
+    data: [
+        {title: ""}
+    ]
+});
+
+var http = Titanium.Network.createHTTPClient();
+http.open("GET", "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=sat_taka_dev");
+http.onload = function() {
+    var json = JSON.parse(http.responseText);
+    tableView.data = json.map(function(tweet) {
+        return({title: tweet.text});
+    });
+};
+http.send();
+
+http.onload = function() {
+    var json = JSON.parse(http.responseText);
+    tableView.data = json.map(function(tweet) {
+        var row = Titanium.UI.createTableViewRow({
+            className: "tweet",
+            height: 'auto'
+        });
+
+        row.add(Titanium.UI.createLabel({
+            text: tweet.user.screen_name,
+            top: 8,
+            left: 64,
+            height: 16
+        }));
+
+        row.add(Titanium.UI.createLabel({
+            text: tweet.text,
+            top: 32,
+            left: 64,
+            right: 8,
+            height: 'auto',
+            bottom: 8
+        }));
+
+        row.add(Titanium.UI.createImageView({
+            url: tweet.user.profile_image_url,
+            top: 8,
+            left: 8,
+            width: 48,
+            height: 48
+        }));
+
+        return(row);
+    });
+};
+
+var messageButton = Ti.UI.createButton({
+    systemButton: Titanium.UI.iPhone.SystemButton.ADD
+});
+messageButton.addEventListener(
+    'click',
+    function () {
+        var messageWindow = Ti.UI.createWindow({
+            url: 'message_window.js',
+            title: 'message',
+            backgroundColor: '#fff'
+        });
+        messageWindow.tableView = tableView;
+        messageWindow.open();
+    }
+);
+win1.rightNavButton = messageButton;
+
+win1.add(tableView);
+
+//  add tabs
+tabGroup.addTab(tab1);
+
+// open tab group
+tabGroup.open();
---
+++ Resources/message_window.js
@@ -0,0 +1,84 @@
+var win = Ti.UI.currentWindow;
+var textArea = Ti.UI.createTextArea({
+    height:150,
+    width:300,
+    top:10,
+    font:{fontSize:20},
+    borderWidth:2,
+    borderColor:'#bbb',
+    borderRadius:5
+});
+
+win.add(textArea);
+var postButton = Ti.UI.createButton({
+    top: 170,
+    right: 10,
+    width: 100,
+    height: 44,
+    title: 'POST'
+});
+win.add(postButton);
+
+Ti.include("lib/twitter_api.js");
+//initialization
+var twitterApi = new TwitterApi({
+    consumerKey:'2Z3ZD3PKTMluWjMdxT0pQ',
+    consumerSecret:'U4dTez6yCmu4IIN07a1yEGMy2vlhjhQBmY5dqZpysAw'
+});
+twitterApi.init(); 
+
+function tweet(message) {
+    var params = {status: message};
+    //status update
+    twitterApi.statuses_update({
+        onSuccess: function(responce){
+            var row = Titanium.UI.createTableViewRow({
+                className: "tweet",
+                height: 'auto'
+            });
+
+            
+            row.add(Titanium.UI.createLabel({
+                text: responce.user.screen_name,
+                top: 8,
+                left: 64,
+                height: 16
+            }));
+            
+
+            row.add(Titanium.UI.createLabel({
+                text: message,
+                top: 32,
+                left: 64,
+                right: 8,
+                height: 'auto',
+                bottom: 8
+                
+            }));
+
+            row.add(Titanium.UI.createImageView({
+                url: responce.user.profile_image_url,
+                top: 8,
+                left: 8,
+                width: 48,
+                height: 48
+            }));
+            
+            win.tableView.insertRowBefore(0, row, {animationStyle:Titanium.UI.iPhone.RowAnimationStyle.DOWN});
+        },
+        onError: function(error){
+            Ti.API.error(error);
+        },
+        parameters:params
+    });
+}
+
+postButton.addEventListener(
+    'click',
+    function () {
+        if (textArea.value) {
+            tweet(textArea.value);
+            win.close({animated:true});
+        }
+    }
+);
